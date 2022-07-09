d7299d054bbfd5b17d8faf700ef6b83b22482aa8
jacobasuncion <asuncion1173@gmail.com>
2022-07-09 15:03:54 +0800

    Added datagather variable and deserializing of json file

--- PdfResumeCreatorVB/Form1.vb
+++ PdfResumeCreatorVB/Form1.vb
@@ -19,5 +19,33 @@ Public Class Form1
 
     Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
 
+        Dim openJson As String = File.ReadAllText(location)
+        Dim finalJson As datagather = JsonConvert.DeserializeObject(Of datagather)(openJson)
+        Dim pdfFile As Document = New Document()
+        PdfWriter.GetInstance(pdfFile, New FileStream("@C:\Users\ASUNCION\source\repos\VB\PdfResumeCreatorVB\ASUNCION_JACOB.pdf", FileMode.Create))
+        pdfFile.Open()
+
+
+
+
     End Sub
+    Public Class datagather
+        Public Property FullName As String
+        Public Property Address As String
+        Public Property Email As String
+        Public Property Number As String
+        Public Property EducationTitle As String
+        Public Property Education1 As String
+        Public Property Education2 As String
+        Public Property WorkExperienceTitle As String
+        Public Property WorkExperience1 As String
+        Public Property SkillsTitle As String
+        Public Property Skill1 As String
+        Public Property Skill2 As String
+        Public Property Skill3 As String
+        Public Property Signature As String
+        Public Property CareerTitle As String
+        Public Property Career As String
+    End Class
+
 End Class
